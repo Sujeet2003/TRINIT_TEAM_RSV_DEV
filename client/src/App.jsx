70b85c0acceb372f32f4d4ec85d739bf46963108
vanamuthuV <vanamuthuvana22@gmail.com>
2024-03-08 19:14:19 +0530

    Initiated Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LandingPage } from "./pages/landingpage";
+import { DashBoard } from "./pages/dashboard";
 
 function App() {
-  
   return (
-    <>
-      <div className='bg-purple-950'>
-        <p className='font-medium text-3xl text-white'>LET THE HACK BEGIN!</p>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
