11104cdfb3ba4c8d4c002046a06080b783ce588e
vanamuthuV <vanamuthuvana22@gmail.com>
2024-03-09 09:56:21 +0530

    Ointer Fixed

--- server/index.js
+++ server/index.js
@@ -19,28 +19,34 @@ const upload = multer({ storage: storage });
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    try {
-        const booksInfo = await pool.query("SELECT * FROM books WHERE id=$1", [1]);
-        res.status(200).json({data : booksInfo})
-    } catch (error) {
-        console.error(error.message);
-    }
-});
-
-app.post("/upload", upload.single("pdfFile"), (req, res) => {
   try {
-    const fileBuffer = req.file.buffer;
-    console.log("File received:");
-    // Here, you can process the fileBuffer (which contains the uploaded PDF file)
-    
-    // For demonstration purposes, we'll send back a simple response with the file name
-    const fileName = req.file.originalname;
-    res.json({ fileName });
+    const booksInfo = await pool.query("SELECT * FROM books WHERE id=$1", [1]);
+    res.status(200).json({ data: booksInfo });
   } catch (error) {
-    console.error("Error handling file upload:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error(error.message);
   }
 });
+
+// app.post("/upload", upload.single("pdfFile"), (req, res) => {
+//   try {
+//     const fileBuffer = req.file.buffer;
+//     console.log("File received:");
+//     // Here, you can process the fileBuffer (which contains the uploaded PDF file)
+
+//     // For demonstration purposes, we'll send back a simple response with the file name
+//     const fileName = req.file.originalname;
+//     res.json({ fileName });
+//   } catch (error) {
+//     console.error("Error handling file upload:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+app.post('/upload', async (req, res) => {
+  console.log("Hello");
+  res.status(200).json({data : "success"})
+})
+
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 5000");
 });
