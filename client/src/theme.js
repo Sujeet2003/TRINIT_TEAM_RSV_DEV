ba732e993b7cf2bc0dfed6019f4103565cc05251
Roopsagar K <roopsagarudayar9854@gamil.com>
2024-03-09 07:22:18 +0530

    theme

---
+++ client/src/theme.js
@@ -0,0 +1,15 @@
+// theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2196f3", // Replace with your primary color
+    },
+    secondary: {
+      main: "#f50057", // Replace with your secondary color
+    },
+  },
+});
+
+export default theme;
